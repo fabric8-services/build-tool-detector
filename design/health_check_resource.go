755f48709b944cd5209ef4bc9f7deae3f1308992
tinakurian <tkurian@redhat.com>
2018-11-07 09:41:07 -0500

    adding health api

---
+++ design/health_check_resource.go
@@ -0,0 +1,21 @@
+/*
+
+Package design is used to develop
+the REST endpoints for the build tool.
+
+*/
+package design
+
+import (
+	a "github.com/goadesign/goa/design/apidsl"
+)
+
+// Endpoint to serve the swagger specification to use
+// with a swagger editor: https://editor.swagger.io/
+var _ = a.Resource("health", func() {
+	a.Origin("*", func() {
+		// Allow all origins to retrieve the Swagger JSON (CORS)
+		a.Methods("GET")
+	})
+	a.Files("/swagger.json", "swagger/swagger.json")
+})
