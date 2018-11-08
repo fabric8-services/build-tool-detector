6f65ecd71f71c32035499056a96c17e098fd3189
tinakurian <tkurian@redhat.com>
2018-11-08 10:07:14 -0500

    from config

--- design/api_definition.go
+++ design/api_definition.go
@@ -7,7 +7,11 @@ the REST endpoints for the build tool.
 package design
 
 import (
+	"github.com/fabric8-services/build-tool-detector/config"
+	"github.com/fabric8-services/build-tool-detector/log"
 	a "github.com/goadesign/goa/design/apidsl"
+	"github.com/spf13/viper"
+	"strconv"
 )
 
 var _ = a.API("build-tool-detector", func() {
@@ -22,7 +26,7 @@ var _ = a.API("build-tool-detector", func() {
 	})
 
 	a.Scheme("http")
-	a.Host("localhost:8099")
+	a.Host(getHost())
 	a.Version("1.0")
 	a.BasePath("/build-tool-detector")
 
@@ -36,3 +40,19 @@ var _ = a.API("build-tool-detector", func() {
 		a.Header("Authorization")
 	})
 })
+
+func getHost() string {
+	viper.SetConfigName("config")
+	viper.AddConfigPath(".")
+	var configuration config.Configuration
+
+	if err := viper.ReadInConfig(); err != nil {
+		log.Logger().Fatalf("Error reading config file, %s", err)
+	}
+	err := viper.Unmarshal(&configuration)
+	if err != nil {
+		log.Logger().Fatalf("unable to decode into struct, %v", err)
+	}
+
+	return configuration.Server.Host + strconv.Itoa(configuration.Server.Port)
+}
