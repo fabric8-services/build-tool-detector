5be1b86fe0da0216a1a852a4ead71ba857f4141a
tinakurian <tkurian@redhat.com>
2018-11-09 14:39:01 -0500

    removing print

--- controllers/status.go
+++ controllers/status.go
@@ -1,8 +1,6 @@
 package controllers
 
 import (
-	"fmt"
-
 	"github.com/fabric8-services/build-tool-detector/app"
 	"github.com/goadesign/goa"
 )
@@ -19,12 +17,5 @@ func NewStatusController(service *goa.Service) *StatusController {
 
 // Show runs the show action.
 func (c *StatusController) Show(ctx *app.ShowStatusContext) error {
-	// StatusController_Show: start_implement
-
-	// Put your logic here
-
-	// StatusController_Show: end_implement
-	res := &app.Status{Commit: app.Commit, BuildTime: app.BuildTime, StartTime: app.StartTime}
-	fmt.Printf("\n\n\n Status: %v \n\n\n", res)
-	return ctx.OK(res)
+	return ctx.OK(&app.Status{Commit: app.Commit, BuildTime: app.BuildTime, StartTime: app.StartTime})
 }
